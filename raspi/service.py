ccabf12afb0e63c8632951e1d80b6d7eda2f78e6
Wene <wene83@gmx.ch>
2021-03-07 16:59:40 +0100

    encapsulate system control in own class

--- raspi/service.py
+++ raspi/service.py
@@ -2,30 +2,80 @@
 
 from PyQt5.QtCore import *
 from PyQt5.QtSerialPort import *
+import signal
+
+
+class TimeKeeperService(QObject):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.serial = SerialInterface(self)
+
+        # timer to give focus to the Python interpreter for processing signals
+        self.python_signal_timer = QTimer()
+        self.python_signal_timer.setInterval(1000)
+        self.python_signal_timer.timeout.connect(lambda: None)
+
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+
+        self.reopen_timer = QTimer()
+        self.reopen_timer.setInterval(3000)
+        self.reopen_timer.timeout.connect(self.open_serial)
+
+    def signal_handler(self, sig_num, stack_frame):
+        self.stop()
+        print('signal handled')
+        QCoreApplication.quit()
+
+    @pyqtSlot()
+    def start(self):
+        self.python_signal_timer.start()
+        self.open_serial()
+
+    @pyqtSlot()
+    def open_serial(self):
+        success = self.serial.open()
+        if success:
+            self.reopen_timer.stop()
+            print('serial opened successfully')
+        else:
+            self.reopen_timer.start()
+            print('could not open serial')
+
+    @pyqtSlot()
+    def stop(self):
+        self.serial.close()
 
 
 class SerialInterface(QObject):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.port = QSerialPort()
-        self.open = False
+        self.is_open = False
 
-    def init(self, name='FT232R USB UART'):
-        if self.open:
+    def open(self, name='FT232R USB UART'):
+        if self.is_open:
             self.port.close()
-            self.open = False
+            self.is_open = False
         all_ports = QSerialPortInfo.availablePorts()
         for info in all_ports:
             if info.description() == name:
                 self.port.setPort(info)
                 self.port.setBaudRate(9600)
                 self.port.open(QSerialPort.ReadWrite)
-                self.open = True
+                self.is_open = True
                 self.port.readyRead.connect(self.read_and_print)
                 break
-        if not self.open:
-            print('Serial port not found')
-            QCoreApplication.quit()
+        if self.is_open:
+            return True
+        else:
+            return False
+
+    def close(self):
+        if self.is_open:
+            self.port.close()
+            self.is_open = False
 
     def read_and_print(self):
         byte_array = self.port.readAll()
@@ -34,25 +84,13 @@ class SerialInterface(QObject):
 
 if '__main__' == __name__:
     import sys
-    import signal
 
     app = QCoreApplication(sys.argv)
 
     app.setOrganizationName('Wene')
     app.setApplicationName('TimeKeeper_service')
 
-    serial_interface = SerialInterface()
-    QTimer.singleShot(0, serial_interface.init)     # init as soon as the event loop is running
-
-    # interrupt the Qt event loop every second and allow Python to handle OS signals
-    timer = QTimer()
-    timer.timeout.connect(lambda: None)
-    timer.start(1000)
-
-    def on_quit(sig_num, stack_frame):
-        app.quit()
-
-    signal.signal(signal.SIGTERM, on_quit)
-    signal.signal(signal.SIGINT, on_quit)
+    service = TimeKeeperService()
+    QTimer.singleShot(0, service.start)     # start as soon as the event loop is running
 
     sys.exit(app.exec_())
