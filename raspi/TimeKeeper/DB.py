f8a896ed8a7ee1feb440bf37fa24d14205921baa
Wene <wene83@gmx.ch>
2021-05-08 13:12:37 +0200

    add some db functions

--- raspi/TimeKeeper/DB.py
+++ raspi/TimeKeeper/DB.py
@@ -18,7 +18,7 @@ class DB(QObject):
         cur.execute('CREATE TABLE IF NOT EXISTS "owner" ("id" INTEGER PRIMARY KEY, "name" TEXT NOT NULL);')
         cur.execute('CREATE TABLE IF NOT EXISTS "source" ("id" INTEGER PRIMARY KEY, "name" TEXT NOT NULL);')
         cur.execute('CREATE TABLE IF NOT EXISTS "badge" ("id" INTEGER PRIMARY KEY, "badge_hex" TEXT NOT NULL, '
-                    '"owner_id" INTEGER NOT NULL, "valid_since" INTEGER, "valid_until" INTEGER);')
+                    '"owner_id" INTEGER NOT NULL, "valid_since" INTEGER);')
         cur.execute('CREATE TABLE IF NOT EXISTS "event" ("id" INTEGER PRIMARY KEY, "badge_hex" TEXT NOT NULL, '
                     '"time" INTEGER NOT NULL, "source_id" INTEGER NOT NULL);')
         cur.close()
@@ -29,7 +29,7 @@ class DB(QObject):
         cur.execute('SELECT "id" FROM "source" WHERE "name" = ?', (source,))
         result = cur.fetchone()
         if result is None:
-            cur.execute('INSERT INTO source (name) VALUES (?)', (source,))
+            cur.execute('INSERT INTO "source" ("name") VALUES (?)', (source,))
             cur.execute('SELECT last_insert_rowid()')
             result = cur.fetchone()
         source_id = result[0]
@@ -37,3 +37,31 @@ class DB(QObject):
         cur.close()
         self.conn.commit()
 
+    def get_owner_id_by_nme(self, name: str):
+        cur = self.conn.cursor()
+        cur.execute('SELECT "id" FROM "owner" WHERE "name" = ?', (name,))
+        result = cur.fetchone()
+        if result is not None:
+            existing_id = result[0]
+            return existing_id
+        cur.execute('INSERT INTO "owner" ("name") VALUES (?)', (name,))
+        cur.execute('SELECT last_insert_rowid()')
+        result = cur.fetchone()
+        new_id = result[0]
+        return new_id
+
+    def add_badge_by_owner_id(self, badge_hex: str, owner_id: int, valid_since: int = None):
+        cur = self.conn.cursor()
+        if valid_since is None:
+            valid_since = QDateTime.currentDateTime().toSecsSinceEpoch()
+        cur.execute('INSERT INTO "badge" ("owner_id", "badge_hex", "valid_since") VALUES (?, ?, ?)',
+                    (owner_id, badge_hex, valid_since))
+        cur.execute('SELECT last_insert_rowid()')
+        result = cur.fetchone()
+        new_id = result[0]
+        return new_id
+
+    def add_badge_by_name(self, badge_hex: str, owner: str, valid_since: int = None):
+        owner_id = self.get_owner_id_by_nme(owner)
+        new_id = self.add_badge_by_owner_id(badge_hex, owner_id, valid_since)
+        return new_id
