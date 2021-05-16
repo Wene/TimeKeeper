135e498eb883c1890494cd068f5a13e0045c2cde
Wene <wene83@gmx.ch>
2021-05-16 15:36:33 +0200

    start tcp server and accept connections

--- raspi/TimeKeeper/Server.py
+++ raspi/TimeKeeper/Server.py
@@ -4,13 +4,39 @@ from PyQt5.QtCore import *
 from PyQt5.QtNetwork import *
 
 
+class Connection(QObject):
+    def __init__(self, socket: QTcpSocket, parent=None):
+        super().__init__(parent)
+        self.socket = socket
+        self.socket.readyRead.connect(self.read)
+        self.socket.disconnected.connect(self.cleanup)
+
+    @pyqtSlot()
+    def read(self):
+        size = self.socket.bytesAvailable()
+        data = self.socket.read(size)
+
+    @pyqtSlot()
+    def cleanup(self):
+        self.deleteLater()
+
+    @pyqtSlot()
+    def close(self):
+        self.socket.close()
+
+
 class Server(QObject):
     def __init__(self, name: str, parent=None):
         super().__init__(parent)
         self.mcast_listener = QUdpSocket(self)
         self.mcast_listener.bind(QHostAddress.Any, 9363)
         self.mcast_listener.readyRead.connect(self.incoming)
+
         self.name = name
+
+        self.tcp_server = QTcpServer(self)
+        self.tcp_server.listen(QHostAddress.Any, 9363)
+        self.tcp_server.newConnection.connect(self.connect)
 
     @pyqtSlot()
     def incoming(self):
@@ -22,3 +48,9 @@ class Server(QObject):
             print(f'got "{text.rstrip()}" from [{sender.toString()}]:{port}')
             if 'I\'m looking for TimeKeeper hosts.' == text:
                 self.mcast_listener.writeDatagram(f'Hi, this is TimeKeeper {self.name}'.encode('utf8'), sender, port)
+
+    @pyqtSlot()
+    def connect(self):
+        while self.tcp_server.hasPendingConnections():
+            socket = self.tcp_server.nextPendingConnection()
+            Connection(socket, self)    # by setting self as parent, this instance will last until self gets deleted
