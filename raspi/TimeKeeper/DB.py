3e173493a9c34481a4d2dfa72ff961f8eea60cad
Wene <wene83@gmx.ch>
2021-06-06 13:15:41 +0200

    normalize database scheme

--- raspi/TimeKeeper/DB.py
+++ raspi/TimeKeeper/DB.py
@@ -18,9 +18,10 @@ class DB(QObject):
         cur = self.conn.cursor()
         cur.execute('CREATE TABLE IF NOT EXISTS "owner" ("id" INTEGER PRIMARY KEY, "name" TEXT NOT NULL);')
         cur.execute('CREATE TABLE IF NOT EXISTS "source" ("id" INTEGER PRIMARY KEY, "name" TEXT NOT NULL);')
-        cur.execute('CREATE TABLE IF NOT EXISTS "badge" ("id" INTEGER PRIMARY KEY, "badge_hex" TEXT NOT NULL, '
-                    '"owner_id" INTEGER NOT NULL, "valid_since" INTEGER);')
-        cur.execute('CREATE TABLE IF NOT EXISTS "event" ("id" INTEGER PRIMARY KEY, "badge_hex" TEXT NOT NULL, '
+        cur.execute('CREATE TABLE IF NOT EXISTS "badge" ("id" INTEGER PRIMARY KEY, "hex" TEXT NOT NULL);')
+        cur.execute('CREATE TABLE IF NOT EXISTS "badge_owner" ("id" INTEGER  PRIMARY KEY, "owner_id" INTEGER NOT NULL, '
+                    '"badge_id" INTEGER NOT NULL, "valid_since" INTEGER NOT NULL);')
+        cur.execute('CREATE TABLE IF NOT EXISTS "event" ("id" INTEGER PRIMARY KEY, "badge_id" INTEGER NOT NULL, '
                     '"time" INTEGER NOT NULL, "source_id" INTEGER NOT NULL);')
         cur.close()
         self.conn.commit()
@@ -35,28 +36,46 @@ class DB(QObject):
             result = cur.fetchone()
         source_id = result[0]
         cur.close()
+        self.conn.commit()
         return source_id
 
-    def event_exists(self, timestamp: int, badge: str):
+    def get_badge_id(self, badge_hex: str):
+        cur = self.conn.cursor()
+        cur.execute('SELECT "id" FROM "badge" WHERE "hex" = ?;', (badge_hex, ))
+        result = cur.fetchone()
+        if result:
+            badge_id = result[0]
+        else:
+            cur.execute('INSERT INTO "badge" ("hex") VALUES (?);', (badge_hex, ))
+            cur.execute('SELECT last_insert_rowid()')
+            result = cur.fetchone()
+            badge_id = result[0]
+        cur.close()
+        self.conn.commit()
+        return badge_id
+
+    def event_exists(self, timestamp: int, badge_id: int):
         existing = False
         cur = self.conn.cursor()
-        cur.execute('SELECT id FROM event WHERE badge_hex = ? AND time = ?', (badge, timestamp))
+        cur.execute('SELECT "id" FROM "event" WHERE "badge_id" =  ? '
+                    'AND "event"."time" = ?', (badge_id, timestamp))
         result = cur.fetchone()
         if result:
             existing = True
         cur.close()
         return existing
 
-    def log_event(self, timestamp: int, badge: str, source: str):
+    def log_event(self, timestamp: int, badge_hex: str, source: str):
         source_id = self.get_source_id(source)
-        if not self.event_exists(timestamp, badge):
+        badge_id = self.get_badge_id(badge_hex)
+        if not self.event_exists(timestamp, badge_id):
             cur = self.conn.cursor()
-            cur.execute('INSERT INTO event (badge_hex, time, source_id) VALUES (?, ?, ?)',
-                        (badge, timestamp, source_id))
+            cur.execute('INSERT INTO event (badge_id, time, source_id) VALUES (?, ?, ?)',
+                        (badge_id, timestamp, source_id))
             cur.close()
             self.conn.commit()
 
-    def get_owner_id_by_nme(self, name: str):
+    def get_owner_id_by_name(self, name: str):
         cur = self.conn.cursor()
         cur.execute('SELECT "id" FROM "owner" WHERE "name" = ?', (name,))
         result = cur.fetchone()
@@ -71,34 +90,36 @@ class DB(QObject):
         self.conn.commit()
         return new_id
 
-    def add_badge_by_owner_id(self, badge_hex: str, owner_id: int, valid_since: int = None):
+    def add_badge_by_owner_id(self, badge_id: int, owner_id: int, valid_since: int = None):
         cur = self.conn.cursor()
         if valid_since is None:
             valid_since = QDateTime.currentDateTime().toSecsSinceEpoch()
 
         # Check for existing, still valid badge
-        cur.execute('SELECT "id" from "badge" '
-                    'WHERE "badge_hex" = ? AND "owner_id" = ? AND "valid_since" <= ? '
+        cur.execute('SELECT "id" from "badge_owner" '
+                    'WHERE "badge_id" = ? AND "owner_id" = ? AND "valid_since" <= ? '
                     'ORDER BY "valid_since" DESC '
-                    'LIMIT 1;', (badge_hex, owner_id, valid_since))
+                    'LIMIT 1;', (badge_id, owner_id, valid_since))
         result = cur.fetchall()
 
         # Only add a new owner if not already valid
         if not result:
-            cur.execute('INSERT INTO "badge" ("owner_id", "badge_hex", "valid_since") VALUES (?, ?, ?)',
-                        (owner_id, badge_hex, valid_since))
+            cur.execute('INSERT INTO "badge_owner" ("owner_id", "badge_id", "valid_since") VALUES (?, ?, ?)',
+                        (owner_id, badge_id, valid_since))
         cur.close()
         self.conn.commit()
 
     def add_badge_by_name(self, badge_hex: str, owner: str, valid_since: int = None):
-        owner_id = self.get_owner_id_by_nme(owner)
-        self.add_badge_by_owner_id(badge_hex, owner_id, valid_since)
+        owner_id = self.get_owner_id_by_name(owner)
+        badge_id = self.get_badge_id(badge_hex)
+        self.add_badge_by_owner_id(badge_id, owner_id, valid_since)
 
     def get_valid_badge_owner(self, badge_hex: str, time: int = 0):
         cur = self.conn.cursor()
-        cur.execute('SELECT owner.name FROM owner, badge '
-                    'WHERE owner.id = badge.owner_id AND badge.badge_hex = ? AND badge.valid_since <= ? '
-                    'ORDER BY badge.valid_since DESC;', (badge_hex, time))
+        cur.execute('SELECT "owner"."name" FROM "owner", "badge", "badge_owner" '
+                    'WHERE "owner"."id" = "badge_owner"."owner_id" AND "badge"."id" = "badge_owner"."badge_id" '
+                    'AND "badge"."hex" = ? AND "badge_owner"."valid_since" <= ? '
+                    'ORDER BY "badge_owner"."valid_since" DESC;', (badge_hex, time))
         result = cur.fetchone()
         cur.close()
 
@@ -108,10 +129,28 @@ class DB(QObject):
         else:
             return badge_hex
 
+    def get_badge_owner_by_id(self, badge_id: int, valid_since: int = None):
+        if valid_since is None:
+            valid_since = QDateTime.currentDateTime().toSecsSinceEpoch()
+        cur = self.conn.cursor()
+        cur.execute('SELECT "owner"."name" FROM "owner", "badge_owner" '
+                    'WHERE "badge_owner"."owner_id" = "owner"."id" '
+                    'AND "badge_owner"."badge_id" = ? AND "badge_owner"."valid_since" <= ? '
+                    'ORDER BY "badge_owner"."valid_since" DESC LIMIT 1;', (badge_id, valid_since))
+        result = cur.fetchone()
+        cur.close()
+        if result:
+            name = result[0]
+            return name
+        else:
+            return None
+
     def get_events_between_timestamps(self, time_from: int, time_to: int):
         cur = self.conn.cursor()
-        cur.execute('SELECT time, badge_hex FROM event WHERE time >= ? AND time <= ? '
-                    'ORDER BY time;', (time_from, time_to))
+        cur.execute('SELECT "event"."time", "badge"."hex" FROM "event", "badge" '
+                    'WHERE "event"."badge_id" = "badge"."id" '
+                    'AND "event"."time" >= ? AND "event"."time" <= ? '
+                    'ORDER BY "event"."time";', (time_from, time_to))
         result = cur.fetchall()
         cur.close()
 
@@ -128,17 +167,21 @@ class DB(QObject):
 
     def get_owners(self):
         cur = self.conn.cursor()
-        cur.execute('SELECT badge_hex, name FROM ' 
-                    '(SELECT * FROM badge, owner WHERE badge.owner_id = owner.id ORDER BY badge.valid_since DESC) '
-                    'GROUP BY badge_hex;')
+        cur.execute('SELECT "id", "hex" FROM "badge" ORDER BY "hex"')
         result = cur.fetchall()
         cur.close()
-        table = []
+
+        owners = []
         for record in result:
-            badge = record[0]
-            name = record[1]
-            table.append(f'{badge} {name}')
-        return table
+            badge_id = record[0]
+            badge_hex = record[1]
+            owner_name = self.get_badge_owner_by_id(badge_id)
+            if owner_name:
+                owners.append(f'{badge_hex} {owner_name}')
+            else:
+                owners.append(badge_hex)
+
+        return owners
 
     @pyqtSlot(NetRequest)
     def answer_net_request(self, request: NetRequest):
